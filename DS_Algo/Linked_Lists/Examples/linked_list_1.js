5c1a906c01b6c577a5f8cd1422a3cf49e2e853b9
atg4691 <atg4691@g.rit.edu>
2019-10-29 14:16:42 -0400

    Continued through List Links

--- DS_Algo/Linked_Lists/Examples/linked_list_1.js
+++ DS_Algo/Linked_Lists/Examples/linked_list_1.js
@@ -1,8 +1,17 @@
-const basket = ['apples', 'grapes', 'pears']
+//const basket = ['apples', 'grapes', 'pears']
 
-linked list: apples --> grapes --> pears
+//linked list: apples --> grapes --> pears
 
-apples
-8947 --> grapes
-          8742 --> pears
-                    372 --> null
+//apples
+//8947 --> grapes
+//          8742 --> pears
+//                    372 --> null
+
+let obj1 = {a: true };
+let obj2 = obj1;      // Points to the same location in memory
+
+obj1.a = 'booya';     // Will change both obj1 and obj2
+//delete obj1           // This will delete the obj1 but obj2 will still keep its place in memory.
+obj2 = 'hello';  
+console.log('1', obj1); // Must comment out if using the delete.
+console.log('2', obj2);

45f9134f6a76ad992f918f873c8a6389c83820de
Andrew Grant <35342691+Rayfall@users.noreply.github.com>
2019-12-06 16:10:02 -0500

    Add files via upload

--- WebDev_Bootcamp/JavaScript_Fun/DOM/index.js
+++ WebDev_Bootcamp/JavaScript_Fun/DOM/index.js
@@ -1,1 +1,23 @@
 alert("Hello");
+
+//Challenge from video 119
+var name = document.querySelector("ul li:nth-of-type(3)").innerHTML = "Andrew";
+
+//var heading = document.firstElementChild.lastElemenetChild.firstElementChild;
+//heading.innerHTML = "Good Bye";
+//heading.style.color = "red";
+
+//document.querySelector("input").click();
+
+/*
+Properties describe the object while methods are what the object can do.
+For example, a get method would be car.drive(); This would get information
+about the car driving. A set method would be setting a value or information
+to the object using a method, such as car.color = "red". Method is something that an object can do.
+
+Properties Examples:
+innerHTML, style, firstChild
+
+Methods:
+click(), appendChild(), setAttribute()
+*/

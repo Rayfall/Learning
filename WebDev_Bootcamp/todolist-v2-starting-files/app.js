f15298c6c109d9a7166fe661f8598cfcb931ddfb
Rayfall <atgrant23@gmail.com>
2020-04-14 18:14:48 -0500

    Finished Todo List

--- WebDev_Bootcamp/todolist-v2-starting-files/app.js
+++ WebDev_Bootcamp/todolist-v2-starting-files/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const _ = require("lodash");
+
 //const date = require(__dirname + "/date.js");
 
 const mongoose = require("mongoose");
@@ -25,6 +27,10 @@ mongoose.connect(mongoURL, {
 }, () => {
   console.log("I'm in...");
 });
+
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+mongoose.set('useFindAndModify', false);
 
 //--------------------------------------------------------
 const todoSchema = new Schema({
@@ -49,6 +55,12 @@ const defaultItems = [item1, item2, item3];
 
 /**/
 
+const listSchema = new Schema({
+  name: String,
+  items: [todoSchema]
+});
+
+const List = mongoose.model("List", listSchema);
 //--------------------------------------------------------
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
@@ -78,19 +90,75 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
+  const item = new Item({
+    name: itemName
+  });
+
+  if(listName === "Today"){
+    item.save();
+  res.redirect("/");
   } else {
-    items.push(item);
-    res.redirect("/");
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
+  
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function(req, res){
+  const checkedItem = req.body.checkBox;
+  const listName = req.body.listName;
+
+  if(listName === "Today") {
+    Item.findByIdAndRemove(checkedItem, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Successfully removed item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      {name: listName},
+      {$pull: {items: {_id: checkedItem}}},
+      function(err, foundList){
+        if(!err){
+          res.redirect("/" + listName);
+        }else{
+          console.log(err);
+        }
+      });
+  }  
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if(!err){
+      if(!foundList){
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      }else{
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }else{
+      console.log(err);
+    }
+  })
+  
+
+  
 });
 
 app.get("/about", function(req, res){

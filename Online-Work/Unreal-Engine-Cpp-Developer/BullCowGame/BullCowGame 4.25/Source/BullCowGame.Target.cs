9221d59d97b4a3016d76b4cf8820fcc582a05b26
Rayfall <atgrant23@gmail.com>
2020-07-02 11:49:59 -0500

    Cleaning up my commits

--- Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGame.Target.cs
+++ Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGame.Target.cs
@@ -7,8 +7,9 @@ public class BullCowGameTarget : TargetRules
 {
 	public BullCowGameTarget(TargetInfo Target) : base(Target)
 	{
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 		Type = TargetType.Game;
-
+		
 		ExtraModuleNames.AddRange( new string[] { "BullCowGame" } );
 	}
 }
--- Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGameEditor.Target.cs
+++ Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGameEditor.Target.cs
@@ -7,8 +7,9 @@ public class BullCowGameEditorTarget : TargetRules
 {
 	public BullCowGameEditorTarget(TargetInfo Target) : base(Target)
 	{
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 		Type = TargetType.Editor;
-
+		
 		ExtraModuleNames.AddRange( new string[] { "BullCowGame" } );
 	}
 }

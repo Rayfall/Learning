d341ae471744800de0c9e5eb0bd687908fc87392
Rayfall <atgrant23@gmail.com>
2020-07-06 15:40:58 -0500

    Updating game text.

--- Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGame/BullCowCartridge.cpp
+++ Online-Work/Unreal-Engine-Cpp-Developer/BullCowGame/BullCowGame 4.25/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,29 +3,51 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();    
-    
-    PrintLine(TEXT("Welcome to Bull & Cows!\nTry guessing the 4 Letter word!"));
-    PrintLine(TEXT("You have 5 to start off with. Every wrong guess will subtract the number of guesses."));
+    Super::BeginPlay();
 
-    InitGame();
+    SetupGame(); 
+
+    //PrintLine(TEXT("The hidden word is: %s, of length: %i"), *HiddenWord, HiddenWord.Len()); // Debug Line    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-
+    
     if(HiddenWord == Input){
         PrintLine(TEXT("Correct!"));
+        EndGame();
     }else{
-        PrintLine(TEXT("Try Again."));
-        PlayerGuesses = PlayerGuesses - 1;
+        if(HiddenWord.Len() != Input.Len()){
+            PrintLine(TEXT("Hidden word is %i Characters long, this does not match."), HiddenWord.Len());
+        }
+
+        if(PlayerGuesses > 0){
+            PlayerGuesses = PlayerGuesses - 1;
+            PrintLine(TEXT("%i"), PlayerGuesses);
+            PrintLine(TEXT("Try Again."));
+        }else{
+            PrintLine(TEXT("You have lost."));
+            EndGame();
+        }
+        
+        
     }
 }
 
-void UBullCowCartridge::InitGame()
+void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("boil");
     PlayerGuesses = 5;
+    bGameOver = false;
 
+    PrintLine(TEXT("Welcome to Bull & Cows!\nTry guessing the %i Letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i guesses to start off with. Every wrong guess will subtract the number of guesses."), PlayerGuesses);
+    PrintLine(TEXT("Type in your guess now and continue by pressing enter!"));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    
 }

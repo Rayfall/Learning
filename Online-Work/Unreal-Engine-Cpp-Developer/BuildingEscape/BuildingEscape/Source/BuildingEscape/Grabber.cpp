1d46cc866a971853c2d313175e5e01dc396fc205
Rayfall <atgrant23@gmail.com>
2020-09-16 13:27:39 -0500

    Finished grabber inputs

--- Online-Work/Unreal-Engine-Cpp-Developer/BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ Online-Work/Unreal-Engine-Cpp-Developer/BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,8 @@
 #include "DrawDebugHelpers.h"
 #include "Engine/World.h"
 #include "GameFramework/PlayerController.h"
+//#include "CollisionQueryParams.h"
+//#include "UObject/NameTypes.h"
 #include "Grabber.h"
 
 #define OUT
@@ -23,8 +25,29 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting for duty!"));
+
+	// Check for Physics Handle Component
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle){
+		UE_LOG(LogTemp, Display, TEXT("Physics handle found: %s"), *GetOwner()->GetName());
+	}
+	else{
+		UE_LOG(LogTemp, Error, TEXT("No Physics handle found on component: %s"), *GetOwner()->GetName());
+	}
+
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if(InputComponent){
+		UE_LOG(LogTemp, Display, TEXT("Input component found: %s"), *GetOwner()->GetName());
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	}
+	else{
+		UE_LOG(LogTemp, Error, TEXT("No Input component found on component: %s"), *GetOwner()->GetName());
+	}
 }
 
+void UGrabber::Grab(){
+	UE_LOG(LogTemp, Display, TEXT("Grabber pressed"));
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -56,6 +79,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	);
 
 	// Ray-cast out to a certain distance (Reach)
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(
+			ECollisionChannel::ECC_PhysicsBody
+		),
+		TraceParams
+	);
+
 	// See what we are hitting
+	AActor* ActorHit = Hit.GetActor();
+
+	if(ActorHit){
+		UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: %s"), *(ActorHit->GetName()));
+	}
+	
 }
 

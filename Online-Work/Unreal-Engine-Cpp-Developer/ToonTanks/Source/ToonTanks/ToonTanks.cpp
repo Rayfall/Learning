2d622785cbc1b6ad13addc70df871ad184516b43
Rayfall <atgrant23@gmail.com>
2020-11-20 07:58:54 -0600

    Uploading recent code changes in Toon Tanks

--- Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/ToonTanks.cpp
+++ Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/ToonTanks.cpp
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ToonTanks.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToonTanks, "ToonTanks" );
-=======
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ToonTanks.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToonTanks, "ToonTanks" );
->>>>>>> 3af449b29ba5c834e3c9e7dd25738669ad595e8b

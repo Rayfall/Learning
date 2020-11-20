dca06028e63f5c2b48dfe9e0efce8fdd12f4fffe
Rayfall <atgrant23@gmail.com>
2020-11-20 13:04:38 -0600

    Started Turret logic

--- Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/Pawns/PawnTurret.cpp
+++ Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -2,4 +2,24 @@
 
 
 #include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
 
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    GetWorld()->GetTimerManager().SetTimer(FireRateTimer, this, &APawnTurret::CheckFireCondition); // GetWorldTimerManager(); // Does the Same thing
+
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APawnTurret::CheckFireCondition()
+{
+
+}

33d7828bb77bac010b5ccac11f1ab6a5710c811d
Rayfall <atgrant23@gmail.com>
2020-12-08 14:49:44 -0600

    Implemented Game modes with start and end cond.

--- Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/GameModes/TankGameModeBass.h
+++ Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/GameModes/TankGameModeBass.h
@@ -6,9 +6,9 @@
 #include "GameFramework/GameModeBase.h"
 #include "TankGameModeBass.generated.h"
 
-/**
- * 
- */
+class APawnTurret;
+class APawnTank;
+
 UCLASS()
 class TOONTANKS_API ATankGameModeBass : public AGameModeBase
 {
@@ -16,8 +16,13 @@ class TOONTANKS_API ATankGameModeBass : public AGameModeBase
 	
 private:
 
+	APawnTank* PlayerTank;
+	int32 TargetTurrets = 0;
+
+	int32 GetTargetTurretCount();
 	void HandleGameStart();
 	void HandleGameOver(bool PlayerWon);
+	
 
 public:
 
@@ -26,6 +31,9 @@ public:
 protected:
 
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Game Loop", meta = (AllowPrivateAccess = "true"))
+	int32 CountDownTimer = 3;
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void GameStart();
--- Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
+++ Online-Work/Unreal-Engine-Cpp-Developer/ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
@@ -31,11 +31,14 @@ private:
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
+	
+	void PawnDestroyed();
+	virtual void HandleDestruction();
 
 protected:
 	void RotateTurret(FVector LookAtTarget);
 	void Fire();
-	virtual void HandleDestruction();
+	
 };
 
 	/*UPROPERTY(VisibleAnywhere, Category = "DEMO")
